d51a34158bb7dfd0b344fe006688503ac7a58693
baabak aghaei <baabakaghaei@gmail.com>
2025-05-10 20:37:11 +0330

    compeleting Crud

--- MinimalApi_Training/Program.cs
+++ MinimalApi_Training/Program.cs
@@ -1,10 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi_Training;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddDbContext<ToDoDb>(opt =>
+{
+    opt.UseInMemoryDatabase("TodoList");
+});
+ 
 
 var app = builder.Build();
+app.MapGet("/todoitems", async (ToDoDb db) => await db.DoItems.ToListAsync());
 
-
+app.MapGet("/todoitems/{id}", async (int id, ToDoDb db) =>
+{
+    await db.DoItems.FindAsync(id);
+});
+app.MapPost("/todoitems", async (ToDoItem item, ToDoDb db) =>
+{
+    db.DoItems.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{item.Id}", item);  
+});
+app.MapPut("/todoitems/{id}", async (int id, ToDoItem inputItem, ToDoDb db) =>
+{
+    var item = await db.DoItems.FindAsync(id);
+    if (item is null) return Results.NotFound();
+    item.Name = inputItem.Name;
+    item.IsCompleted = inputItem.IsCompleted;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/todoitems/{id}", async (int id, ToDoDb db) =>
+{
+    var item = await db.DoItems.FindAsync(id);
+    if (item is null) return Results.NotFound();
+    db.DoItems.Remove(item);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
 
---
+++ MinimalApi_Training/TodoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi_Training
+{
+    public class ToDoDb:DbContext
+    {
+        public ToDoDb(DbContextOptions<ToDoDb> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDoItem> DoItems { get; set; }
+
+    }
+}
